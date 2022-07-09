faab2b4b2329aab46a6dd1c56f454e17cedd119b
Dev Aggarwal <contact@aggarwal.dev>
2022-07-09 17:23:49 +0530

    feat: read application port from config service

--- apps/hello-world/src/main.ts
+++ apps/hello-world/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { HelloWorldModule } from './hello-world.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(HelloWorldModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<string>('PORT'));
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
