581238f202a56a2fb77f6148ea20c3977cdcf7b1
Dev Aggarwal <contact@aggarwal.dev>
2022-07-09 17:22:34 +0530

    feat: add configModule and configuration validation schema

--- apps/hello-world/src/hello-world.module.ts
+++ apps/hello-world/src/hello-world.module.ts
@@ -1,9 +1,40 @@
+import * as Joi from 'joi';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { HelloWorldController } from './hello-world.controller';
 import { HelloWorldService } from './hello-world.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().empty('').default(3000),
+        DATABASE_URL: Joi.alternatives([
+          Joi.string().ip({ cidr: 'forbidden' }),
+          Joi.string().hostname(),
+        ])
+          .empty('')
+          .default('localhost'),
+        DATABASE_PORT: Joi.number().empty('').default(3306),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASS: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PREFIX: Joi.string().empty('').default('nm_'),
+        REDIS_HOST: Joi.alternatives([
+          Joi.string().ip({ cidr: 'forbidden' }),
+          Joi.string().hostname(),
+        ])
+          .empty('')
+          .default('localhost'),
+        REDIS_PORT: Joi.number().empty('').default(6379),
+      }),
+    }),
+  ],
   controllers: [HelloWorldController],
   providers: [HelloWorldService],
 })
