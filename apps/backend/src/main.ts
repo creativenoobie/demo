1e01052e6cb4a752db3b73aa660ca81fc4a8d6f0
Dev Aggarwal <contact@aggarwal.dev>
2022-07-12 21:31:18 +0530

    feat: configure swagger documentation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { BackendModule } from './backend.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BackendModule);
 
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Now&Me')
+    .setDescription('The Now&Me API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get<string>('PORT'));
 }
