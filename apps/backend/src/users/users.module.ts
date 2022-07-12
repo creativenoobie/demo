35fcc8d31e67372fedae67cce853ee99f8df1b85
Dev Aggarwal <contact@aggarwal.dev>
2022-07-12 14:38:34 +0530

    feat: user service

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entity/user.entity';
 import { TokenService } from './token.service';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [],
-  providers: [TokenService],
+  providers: [TokenService, UserService],
 })
 export class UsersModule {}
